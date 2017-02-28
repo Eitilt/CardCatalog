e77bfcb7c1719da76e977259692076384a4e19c0
Eitilt <ag.eitilt@gmail.com>
2017-02-28 14:43:56 -0800

    Split ConcatStream into its own (future) submodule

--- Metadata/Metadata.Test/Program.fs
+++ Metadata/Metadata.Test/Program.fs
@@ -1,12 +1,9 @@
 ﻿open FsCheck
 
-open Generators
+open Metadata.Test.Generators
 
 [<EntryPoint>]
 let main argv =
-    Gen.sample 5 5 Arb.generate<ReadableStream>
-    |> printfn "%A"
-    
     printfn "Press any key to close..."
     System.Console.ReadKey () |> ignore
 
