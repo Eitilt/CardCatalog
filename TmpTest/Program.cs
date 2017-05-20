63d844608f2bba9946fe3de1a93dcab3079a22cc
Sam May <ag.eitilt@gmail.com>
2017-05-19 18:38:49 -0700

    Provide minimal framework for debugging May be safely removed once F# supports MSBuild 15 ProjectReferences

---
+++ TmpTest/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TmpTest {
+	class Program {
+		static void Main(string[] args) {
+			AgEitilt.CardCatalog.FormatRegistry.RegisterAll<AgEitilt.CardCatalog.Audio.AudioTagFormat>();
+			var _ = AgEitilt.CardCatalog.FormatRegistry.ParseAsync(System.IO.File.OpenRead(@"..\..\..\..\..\..\Shared\Music\Malukah\Misty Mountains.mp3")).Result;
+		}
+	}
+}
