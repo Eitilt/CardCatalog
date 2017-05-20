721339f6d06dd26842d3a9fdfcbbcfd57949d0b4
Sam May <ag.eitilt@gmail.com>
2017-05-19 18:34:07 -0700

    XAML requires bound values be properties

--- Core/ImageData.cs
+++ Core/ImageData.cs
@@ -6,11 +6,11 @@
 		/// <summary>
 		/// The encoding of this image, as a standard MIME type string.
 		/// </summary>
-		public string Type;
+		public string Type { get; set; }
 
 		/// <summary>
 		/// The raw binary data describing the image.
 		/// </summary>
-		public byte[] Data;
+		public byte[] Data { get; set; }
 	}
 }
